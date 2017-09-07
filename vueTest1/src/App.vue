4f141fb691c1dd6beb5b0a66a243a700a1a04440
SKY-20170221JMY\Administrator <844274705@qq.com>
2017-09-07 11:48:08 +0800

    add v1.0

---
+++ vueTest1/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view></router-view>
+    <firstcomponent></firstcomponent>
+  </div>
+</template>
+
+<script>
+import firstcomponent from './components/firstcomponents.vue'
+
+export default {
+  name: 'app',
+   data () {
+    return {
+      msg: 'Hello Vue!'
+    }
+  },
+  components: { firstcomponent }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  display: flex;
+}
+</style>
---
+++ vueTest1/src/components/firstcomponents.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="firstCompontents">
+        <h1>This is My FirstCompontents</h1>
+        <a :href="goUrl">{{msg}}</a>
+    </div>
+</template>
+<style>
+    #firstCompontents{
+        background-color: #ff0;
+    }
+</style>
+
+<script>
+export default {
+  data(){
+      return {
+          msg:"点击跳转到百度",
+          goUrl:'http://www.baidu.com'
+      }
+  }
+}
+
+</script>
+
